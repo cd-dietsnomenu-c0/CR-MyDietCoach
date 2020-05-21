0f6e725e6d2d5cf1906d7b3ef14d84b968622148
Wadim <waadimka@mail.ru>
2020-05-21 19:57:45 +0300

    ad native

--- app/src/main/java/com/wsoteam/mydietcoach/diets/controllers/AdVH.kt
+++ app/src/main/java/com/wsoteam/mydietcoach/diets/controllers/AdVH.kt
@@ -13,15 +13,13 @@ import com.wsoteam.mydietcoach.R
 import kotlinx.android.synthetic.main.vh_native.view.*
 
 class AdVH(layoutInflater: LayoutInflater, viewGroup: ViewGroup)
-    : RecyclerView.ViewHolder(layoutInflater.inflate(R.layout.vh_native, viewGroup, false)) {
+    : RecyclerView.ViewHolder(layoutInflater.inflate(R.layout.vh_native_main, viewGroup, false)) {
 
     init {
-        itemView.ad_view.mediaView = itemView.ad_media
         itemView.ad_view.headlineView = itemView.ad_headline
         itemView.ad_view.bodyView = itemView.ad_body
         itemView.ad_view.callToActionView = itemView.ad_call_to_action
         itemView.ad_view.iconView = itemView.ad_icon
-        itemView.ad_view.starRatingView = itemView.ad_stars
     }
 
     fun bind(unifiedNativeAd: UnifiedNativeAd) {
@@ -41,16 +39,6 @@ class AdVH(layoutInflater: LayoutInflater, viewGroup: ViewGroup)
             (itemView.ad_view.iconView as ImageView).setImageDrawable(icon.drawable)
             itemView.ad_view.iconView.visibility = View.VISIBLE
         }
-
-        if (nativeAd.starRating == null) {
-            itemView.ad_view.starRatingView.visibility = View.INVISIBLE
-        } else {
-            (itemView.ad_view.starRatingView as RatingBar).rating = nativeAd.starRating.toFloat()
-            itemView.ad_view.starRatingView.visibility = View.VISIBLE
-        }
-
-
-
         itemView.ad_view.setNativeAd(nativeAd)
     }
 }
