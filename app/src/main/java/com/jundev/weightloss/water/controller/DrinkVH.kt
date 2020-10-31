3cf207b700906089384241ab996fe3013d2d1ab6
Wadim <udovoron@gmail.com>
2020-10-31 20:59:35 +0300

    select anim

--- app/src/main/java/com/jundev/weightloss/water/controller/DrinkVH.kt
+++ app/src/main/java/com/jundev/weightloss/water/controller/DrinkVH.kt
@@ -1,6 +1,7 @@
 package com.jundev.weightloss.water.controller
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -8,24 +9,28 @@ import com.jundev.weightloss.R
 import kotlinx.android.synthetic.main.item_drink.view.*
 import kotlinx.android.synthetic.main.item_eat.view.*
 
-class DrinkVH(layoutInflater: LayoutInflater, viewGroup: ViewGroup) : RecyclerView.ViewHolder(layoutInflater.inflate(R.layout.item_drink, viewGroup, false)) {
+class DrinkVH(layoutInflater: LayoutInflater, viewGroup: ViewGroup) : RecyclerView.ViewHolder(layoutInflater.inflate(R.layout.item_drink, viewGroup, false)), View.OnClickListener {
+
+    init {
+        itemView.setOnClickListener(this)
+        itemView.lavCircleComplete.speed = 5f
+    }
+
     fun bind(imageId : Int, name : String, gradientId : Int, hydroFactor : Int){
-        //itemView.llDrinkParent.setBackgroundResource(getBackground(gradientId))
         Glide.with(itemView.context).load(getImageDrink(imageId)).into(itemView.ivImgDrink)
         itemView.tvDrinkName.text = name
     }
 
-    private fun getBackground(gradientId: Int): Int {
-        return itemView
-                .resources
-                .obtainTypedArray(R.array.water_drinks_gradients)
-                .getResourceId(gradientId, -1)
-    }
 
     private fun getImageDrink(imageId: Int): Int {
         return itemView
                 .resources
                 .obtainTypedArray(R.array.water_drinks_imgs_color)
                 .getResourceId(imageId, -1)
+    }
+
+    override fun onClick(v: View?) {
+        itemView.lavTick.playAnimation()
+        itemView.lavCircleComplete.playAnimation()
     }
 }
