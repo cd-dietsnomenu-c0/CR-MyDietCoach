d4e900814fb9849f075aa384e654dca77a112132
Wadim <udovoron@gmail.com>
2020-11-02 23:51:51 +0300

    add adapter

--- app/src/main/java/com/jundev/weightloss/water/FragmentWaterTracker.kt
+++ app/src/main/java/com/jundev/weightloss/water/FragmentWaterTracker.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.jundev.weightloss.R
 import com.jundev.weightloss.water.controller.DrinkAdapter
 import com.jundev.weightloss.water.controller.IDrinkAdapter
+import com.jundev.weightloss.water.controller.quick.QucikAdapter
 import kotlinx.android.synthetic.main.bottom_water_settings.*
 import kotlinx.android.synthetic.main.fragment_water_tracker.*
 
@@ -16,6 +18,7 @@ class FragmentWaterTracker : Fragment(R.layout.fragment_water_tracker) {
 
     var bsWaterSettings: BottomSheetBehavior<LinearLayout>? = null
     var adapter: DrinkAdapter? = null
+    var quickAdapter: QucikAdapter? = null
     var progress = 0.0f
     var listValues = arrayOf("50 мл", "100 мл", "150 мл", "200 мл",
             "250 мл", "300 мл", "350 мл", "400 мл", "450 мл", "500 мл")
@@ -33,9 +36,10 @@ class FragmentWaterTracker : Fragment(R.layout.fragment_water_tracker) {
         })
         rvDrinks.adapter = adapter
 
-        btn.setOnClickListener {
-            bsWaterSettings!!.state = BottomSheetBehavior.STATE_EXPANDED
-        }
+        rvQuickDrink.layoutManager = GridLayoutManager(activity, 2)
+        quickAdapter = QucikAdapter()
+        rvQuickDrink.adapter = quickAdapter
+
 
         npValues.minValue = 0
         npValues.maxValue = listValues.size
@@ -47,6 +51,8 @@ class FragmentWaterTracker : Fragment(R.layout.fragment_water_tracker) {
                 changeCapacity(view.value)
             }
         }
+
+
 
     }
 
---
+++ app/src/main/java/com/jundev/weightloss/water/controller/quick/QuickVH.kt
@@ -0,0 +1,9 @@
+package com.jundev.weightloss.water.controller.quick
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.jundev.weightloss.R
+
+class QuickVH(layoutInflater: LayoutInflater, viewGroup: ViewGroup) : RecyclerView.ViewHolder(layoutInflater.inflate(R.layout.item_quick, viewGroup, false)) {
+}
