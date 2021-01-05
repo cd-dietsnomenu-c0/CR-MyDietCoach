094bcc593e450ecc368f75a5a66cfedab776172d
Wadim <udovoron@gmail.com>
2021-01-05 19:54:31 +0300

    bs

--- app/src/main/java/com/jundev/weightloss/water/FragmentWaterTracker.kt
+++ app/src/main/java/com/jundev/weightloss/water/FragmentWaterTracker.kt
@@ -1,7 +1,9 @@
 package com.jundev.weightloss.water
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
@@ -11,6 +13,8 @@ import com.jundev.weightloss.R
 import com.jundev.weightloss.utils.PrefWorker
 import com.jundev.weightloss.water.controller.DrinkAdapter
 import com.jundev.weightloss.water.controller.IDrinkAdapter
+import com.jundev.weightloss.water.controller.capacities.CapacityAdapter
+import com.jundev.weightloss.water.controller.capacities.ICapacityAdapter
 import com.jundev.weightloss.water.controller.quick.IQuick
 import com.jundev.weightloss.water.controller.quick.QuickAdapter
 import kotlinx.android.synthetic.main.bottom_water_settings.*
@@ -20,9 +24,10 @@ class FragmentWaterTracker : Fragment(R.layout.fragment_water_tracker) {
 
     var bsWaterSettings: BottomSheetBehavior<LinearLayout>? = null
     var adapter: DrinkAdapter? = null
+    var capacityAdapter: CapacityAdapter? = null
     var quickAdapter: QuickAdapter? = null
     var progress = 0.0f
-    var listValues : Array<String>? = null
+    var listValues: Array<String>? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -30,6 +35,23 @@ class FragmentWaterTracker : Fragment(R.layout.fragment_water_tracker) {
         checkFirstStart()
         fillBS()
         fillQuick()
+
+        lavTraining.frame = 17
+
+        cvTraining.setOnClickListener {
+            lavTraining.playAnimation()
+        }
+
+        bsWaterSettings!!.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback(){
+            override fun onSlide(p0: View, p1: Float) {
+            }
+
+            override fun onStateChanged(p0: View, p1: Int) {
+                if (p1 == BottomSheetBehavior.STATE_COLLAPSED){
+                    ivDimBackground.visibility = View.GONE
+                }
+            }
+        })
     }
 
     private fun fillQuick() {
@@ -40,6 +62,7 @@ class FragmentWaterTracker : Fragment(R.layout.fragment_water_tracker) {
             }
 
             override fun onSettings(position: Int) {
+                ivDimBackground.visibility = View.VISIBLE
                 prepareBS(position, PrefWorker.getQuickData(position)!!, PrefWorker.getCapacityIndex(position)!!)
                 bsWaterSettings!!.state = BottomSheetBehavior.STATE_EXPANDED
             }
@@ -49,8 +72,6 @@ class FragmentWaterTracker : Fragment(R.layout.fragment_water_tracker) {
 
     private fun prepareBS(position: Int, quickData: Int, capacityIndex: Int) {
         adapter!!.selectNew(quickData)
-        npValues.value = capacityIndex + 1
-        changeCapacity(capacityIndex + 1)
         rvDrinks.scrollToPosition(quickData)
     }
 
@@ -80,22 +101,21 @@ class FragmentWaterTracker : Fragment(R.layout.fragment_water_tracker) {
         })
         rvDrinks.adapter = adapter
 
-        npValues.minValue = 0
-        npValues.maxValue = listValues!!.size
-        npValues.displayedValues = listValues
-        npValues.minValue = 1
-
-        npValues.setOnScrollListener { view, scrollState ->
-            if (scrollState == 0) {
-                changeCapacity(view.value)
+        rvCapacities.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        capacityAdapter = CapacityAdapter(resources.getStringArray(R.array.drink_capacity_values), convertImgsIds(resources.getIntArray(R.array.drink_capacity_icons)), object : ICapacityAdapter{
+            override fun select(newSelect: Int, oldSelect: Int) {
+                capacityAdapter!!.unSelect(oldSelect)
             }
-        }
+        }, 1)
+        rvCapacities.adapter = capacityAdapter
+
     }
 
-    private fun changeCapacity(value: Int) {
-        tvCurrentCapacity.text = resources.getStringArray(R.array.drink_capacity_names)[value - 1]
-        ivCapacity.setImageResource(resources
-                .obtainTypedArray(R.array.drink_capacity_icons)
-                .getResourceId(value - 1, -1))
+    private fun convertImgsIds(imgsIds: IntArray): ArrayList<Int> {
+        var convertedImgsIds: ArrayList<Int> = arrayListOf()
+        for (j in imgsIds.indices) {
+            convertedImgsIds.add(resources.obtainTypedArray(R.array.drink_capacity_icons).getResourceId(j, -1))
+        }
+        return convertedImgsIds
     }
 }
---
+++ app/src/main/java/com/jundev/weightloss/water/controller/capacities/ICapacityAdapter.kt
@@ -0,0 +1,5 @@
+package com.jundev.weightloss.water.controller.capacities
+
+interface ICapacityAdapter {
+    fun select(newSelect : Int, oldSelect : Int)
+}
