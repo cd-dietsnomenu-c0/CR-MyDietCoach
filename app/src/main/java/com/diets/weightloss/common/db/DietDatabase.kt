7235c76474edbe8fd13085e0a9b2e3505902aca2
Wadim <40245987+vsalamov@users.noreply.github.com>
2021-05-13 20:56:01 +0300

    fix

---
+++ app/src/main/java/com/diets/weightloss/common/db/DietDatabase.kt
@@ -0,0 +1,16 @@
+package com.diets.weightloss.common.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.diets.weightloss.common.db.dao.DietDAO
+import com.diets.weightloss.common.db.entities.DietPlanEntity
+import com.diets.weightloss.common.db.entities.FavoriteEntity
+import com.diets.weightloss.common.db.entities.water.DrinksCapacities
+import com.diets.weightloss.common.db.entities.water.WaterIntake
+import com.diets.weightloss.common.db.entities.water.WaterRate
+
+@Database(entities = [DietPlanEntity::class, FavoriteEntity::class, WaterIntake::class, DrinksCapacities::class, WaterRate::class], version = 3, exportSchema = false)
+abstract class DietDatabase : RoomDatabase() {
+    abstract fun dietDAO(): DietDAO
+
+}
---
+++ app/src/main/java/com/diets/weightloss/common/db/migrations/Migrations.kt
@@ -0,0 +1,21 @@
+package com.diets.weightloss.common.db.migrations
+
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+object Migrations {
+
+    val migration_1_2 = object : Migration(1,2){
+        override fun migrate(database: SupportSQLiteDatabase) {
+            database.execSQL("create table FavoriteEntity (id int (10) NOT NULL, PRIMARY KEY (id));")
+        }
+    }
+
+    val migration_2_3 = object : Migration(2,3){
+        override fun migrate(database: SupportSQLiteDatabase) {
+            database.execSQL("create table WaterIntake (id int (20) NOT NULL, typeId int (10) NOT NULL, dirtyCapacity int (10) NOT NULL, clearCapacity int (10) NOT NULL, PRIMARY KEY (id));")
+            database.execSQL("create table DrinksCapacities (typeDrink int NOT NULL, dirtyCapacity int NOT NULL, PRIMARY KEY (typeDrink));")
+            database.execSQL("create table WaterRate (timestamp int NOT NULL, rate int NOT NULL, PRIMARY KEY (timestamp));")
+        }
+    }
+}
